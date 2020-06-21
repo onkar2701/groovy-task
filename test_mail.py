b42e164617ba9d3d2eb9f59b9dfad17ada33bccf
onkar <onkarkadam27@gmail.com>
2020-06-22 00:07:53 +0530

    first commit

---
+++ test_mail.py
@@ -0,0 +1,10 @@
+import smtplib
+
+sender_email = 'onkarkadam27@gmail.com'
+receiver = 'sumitpol1995@gmail.com'
+message = "The Website is not running properly."
+server =smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(sender_email,password)
+server.sendmail(sender_email,receiver,message)
+print("Email has been sent to ", receiver)
